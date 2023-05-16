3557aa5c4722794c5e614a829bd67c2aea0239e0
Rodrigo M.V.S <88489374+RodrigoMvs123@users.noreply.github.com>
2023-05-16 14:26:24 -0300

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,61 @@
+const colorPaletes = [
+       [
+              {
+                     altStroke: ‘#1a2f38’,
+                     fill: #’264653’
+              },
+              {
+                     altStroke: ‘#1b4d4c’,
+                     fill: #’287271’
+              },
+              {
+                     altStroke: ‘#1b6265d’,
+                     fill: #’2a9d8f’
+              },
+              {
+                     altStroke: ‘#6b8a60’,
+                     fill: #’8ab17d’
+              },
+              {
+                     altStroke: ‘#898a54’,
+                     fill: #’babb74’
+              },
+              {
+                     altStroke: ‘#ba9d56’,
+                     fill: #’e9c46a’
+              },
+              {
+                     altStroke: ‘#c2804c’,
+                     fill: #’f4a261’
+              },
+             {
+                     altStroke: ‘#b5573f’,
+                     fill: #’e76f51’
+              },
+              {
+                     altStroke: ‘#9e789e’,
+                     fill: #’ba8fba’
+              },
+              {
+                     altStroke: ‘#42466e’,
+                     fill: ‘#595f94’
+              },
+              {
+                     altStroke: ‘#661c0a’,
+                     fill: ’f3a8be’
+              },
+              {
+                     altStroke: ‘#912b11’,
+                     fill: ‘#b43718’
+              },
+       ],
+       [ 
+              {
+                     altStroke: ‘2A415B’,
+                     fill: ‘#355070’
+              },
+              {
+                     altStroke: ‘3A3D53’,
+                     fill: ‘#515575
+              },
+       ]
